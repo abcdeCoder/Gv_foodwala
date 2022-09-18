29fee0da8028b4b2ede42c134b6d1ad5c01f3fbd
Gaurav verma <gaurav.02116403220@ipu.ac.in>
2022-09-18 21:44:01 +0530

    gv foodwala

---
+++ client/build/precache-manifest.0281689ebe4d1ece5626b92aeba6fa45.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b98650e2589f94f5511d19ae92be29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bc24516f49caa6c28ed",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "1be6ff8aecdc73b9e56b",
+    "url": "/static/css/main.a4bc9e50.chunk.css"
+  },
+  {
+    "revision": "9bc24516f49caa6c28ed",
+    "url": "/static/js/2.d02080f8.chunk.js"
+  },
+  {
+    "revision": "2a0b20e1d02e77e8b582ff21a215748a",
+    "url": "/static/js/2.d02080f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be6ff8aecdc73b9e56b",
+    "url": "/static/js/main.50d5c6b8.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  },
+  {
+    "revision": "90dabe9344551a1f7385104e2454b1e9",
+    "url": "/static/media/404.90dabe93.jpg"
+  },
+  {
+    "revision": "42d709e436404ca5c9cad3c6de0c31d6",
+    "url": "/static/media/check.42d709e4.png"
+  },
+  {
+    "revision": "837e0b22921e25f5a2eca9d514a4a2e1",
+    "url": "/static/media/close.837e0b22.png"
+  },
+  {
+    "revision": "e295470bc88c8d972e83f4e3d77f7ea9",
+    "url": "/static/media/empty.e295470b.png"
+  },
+  {
+    "revision": "6293f16266f118ba96ae833d911701db",
+    "url": "/static/media/logo.6293f162.svg"
+  },
+  {
+    "revision": "a8f6abbb4ea76be277e34cbb17ca9175",
+    "url": "/static/media/noItem.a8f6abbb.png"
+  },
+  {
+    "revision": "a4f40ebb79eb57c5ccd32170cc00d38b",
+    "url": "/static/media/profile.a4f40ebb.png"
+  },
+  {
+    "revision": "bc72b72ec06fc6435aad6be9949ed8df",
+    "url": "/static/media/register.bc72b72e.svg"
+  }
+]);
