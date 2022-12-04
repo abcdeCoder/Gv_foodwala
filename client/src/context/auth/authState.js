914da060cf6a1c991ed12059f30371eba585b6ec
Gaurav verma <94301843+abcdeCoder@users.noreply.github.com>
2022-12-04 13:22:15 +0530

    update

--- client/src/context/auth/authState.js
+++ client/src/context/auth/authState.js
@@ -94,7 +94,7 @@ const AuthState = (props) => {
 
     try {
       const res = await axios.post(
-        "https://joyous-teal-rabbit.cyclic.app/api/login",
+        "https://naughty-bear-hosiery.cyclic.app/api/login",
         formData,
         config
       );
